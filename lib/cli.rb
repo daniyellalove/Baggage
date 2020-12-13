faa0d903090842e681a41bc69093497610c42e77
github username <github email address>
2020-12-13 17:01:38 -0600

    working on cli class

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,9 +4,8 @@
 class Baggage::CLI
     def run
         greeting
-        choose_baggage
+        list_baggage_1
         menu
-        change_character?
         goodbye
     end
 
@@ -18,70 +17,40 @@ class Baggage::CLI
         sleep 1
         puts ("Enter 1 for Marvel or 2 for DC") 
         puts "1. Marvel"
-        sleep(1)
+        sleep 0.5
         puts "2. DC"
-        sleep(1)
         input = gets.strip
-        characters = Hash.new()
-        characters["Iron Man"] = "I'm a genius but I have trouble commiting to 1 person"
-        characters["Scarlett Witch"] = "I nearly wiped out all mutant life on earth"
-        characters["Batman"] = "I don't have any powers and I hide my true identity"
-        characters["Catwoman"] = "I'm obsessed with cats and I have low self-control"
+        characters = Hash.new
+            characters["Iron Man"] = "1. I'm a genius but I have trouble commiting to 1 person"
+            characters["Scarlett Witch"] = "2. I nearly wiped out all mutant life on earth"
+            characters["Batman"] = "1. I don't have any powers and I hide my true identity"
+            characters["Catwoman"] = "2. I'm obsessed with cats and I have low self-control"
         if input == "1"
-            puts characters["Iron Man", "Scarlett Witch"]
+            puts "Choose your baggage: "
+            puts ""
+            puts characters["Iron Man"] 
+            sleep(0.5)
+            puts characters["Scarlett Witch"]
+            sleep(0.5)
+            input = gets.strip
+            puts "#{input}" 
         elsif input == "2"
-            puts characters["Batman", "catwoman"]
-        else
-            puts "Go back to main menu"
-            menu
+            puts characters["Batman"]
+            sleep(0.5)
+            puts characters["Catwoman"]
+            sleep(0.5)
+            input = gets.strip
+            puts "#{input}"
         end
-    end
-    
-        #publisher = Baggage::Publisher.find(input.to_i)
-    def change_character?
-        puts ""
-        puts "Would you like to change your character? Enter Y or N" 
-        sleep 1
-        input = gets.strip.downcase
-        if input == "y"
-            menu 
-            sleep 1
-        elsif input == "n"
-            puts "Let's Begin" 
-            sleep 1
-        else 
-            raiseerror 
-        end
-        #|publish, i|
-        #puts "#{i + 1}. #{publish.name}"
     end
 
     def menu
         loop do
         end
-        until input == "exit"
-            return greeting 
-            sleep 1
-        end
-        puts "Choose your baggage: " 
-        sleep 1
-        input = gets.strip.downcase 
-        choose_baggage 
-    end
-
-    def choose_baggage
-        characters = Hash["Iron Man" => "I'm a genius but I have trouble commiting to 1 person",
-        "Scarlett Witch" => "I nearly wiped out all mutant life on earth",
-        "Batman" => "I don't have any powers and I hide my true identity", 
-        "Catwoman" => "I'm obsessed with cats and I have low self-control"]
-        if @input == "1"
-            puts Hash["Iron Man", "Scarlett Witch"]
-        elsif greeting.input == "2"
-            puts Hash["Batman", "Catwoman"]
-        else
-            puts "Go back to main menu"
-            menu
-        end
+        puts "enter exit to exit"
+        input = gets.strip
+        puts "Exiting.."
+        exit
     end
 
 
@@ -95,13 +64,42 @@ class Baggage::CLI
        # end
   # end
 
-   # def list_dc_baggage
-       # baggage_1 = {
-        #    "Harley Quinn" => "I cheated my way into college"
-        #    "Poison Ivy" => "I stole an egyption artifact"
-        #    "Superman" => "I can be a real jerk"
-        #    "Joker" => "I manipulate others for my own good"
-       # }
+   def list_baggage_1
+        puts ""
+        puts "Starting baggage 1... Enter a number to get rid of some baggage"
+        puts ""
+        puts "Harley Quinn says: 1. I cheated my way into college"
+        puts "Joker says 2. I manipulate others for my own good" 
+        puts "Dark Phoenix says: 3. I'm hot headed"
+        puts "Thanos says: 4. I stole a child"  
+        puts "Exit"
+        input = gets.strip
+        if input == "1"
+            puts "You have too much baggage Harley Quinn"
+            puts "Harley Quinn says: I never liked you anyway"
+            sleep 1   
+        elsif input == "2"
+            puts "You have too much baggage Joker"
+            puts "HAahahAha"
+            sleep 1
+        elsif input == "3"
+            puts "You have too much baggage Dark Phoenix"
+            puts "Dark Phoenix says: I never wanted you"
+            sleep 1
+        elsif input == "4"
+            puts "You have too much baggage Thanos"
+            puts "Goodbye loser"
+            sleep 1
+        else
+            puts "Exiting now.." 
+            sleep 1
+            exit
+        end
+    end
+      
+
+
+        
        # baggage_2 = {
         #    "Harley Quinn" => "I killed my ex-boyfriend"
          #   "Poison Ivy" => "I seduce people to get my way"
@@ -118,10 +116,7 @@ class Baggage::CLI
 
     #def list_marvel_baggage
       #  baggage_1 = {
-        #    "Dark Phoenix" => "I'm hot headed"
-         #   "Storm" => "I have severe claustrophobia"
-         #   "Thor" => "I can be really obnoxius"
-          #  "Thanos" => "I stole a child"
+        # 
        # }
        # baggage_2 = {
           #  "Dark Phoenix" => "I'm a sociopath and I faked my own death"
@@ -148,3 +143,9 @@ end
 #make code more abstract
 #use api in cli
 #use graphics
+
+ #puts "Storm" => "I have severe claustrophobia"
+    #puts "Thor" => "I can be really obnoxius"
+
+    #puts "Poison Ivy" => "2. I stole an egyption artifact"
+    #puts "Superman" => "3. I can be a real jerk"
